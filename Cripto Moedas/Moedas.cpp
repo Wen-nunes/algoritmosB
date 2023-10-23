80631b27ee69ac4a2b96a0981a42607d770ab77f
Wendel <138839125+Wen-nunes@users.noreply.github.com>
2023-10-23 19:40:12 -0300

    Add files via upload

---
+++ Cripto Moedas/Moedas.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+#include "struct.h"
+
+#include "metodos.h"
+
+int main() {
+    Criptomoeda* vetor = nullptr;
+    int totalCriptomoedas = 0;
+    int capacidade = 0; 
+
+    carregarCriptomoedas(vetor, totalCriptomoedas, capacidade);
+
+    int opcao;
+
+    cout << "Quantas criptomoedas quer gerenciar: ";
+    cin >> capacidade;
+
+    vetor = new Criptomoeda[capacidade];
+    delete[] vetor; // Liberar memória alocada antes de sair
+
+    do {
+        cout << "MENU" << endl;
+        cout << "1 - Cadastrar Criptomoeda" << endl;
+        cout << "2 - Atualizar Valor" << endl;
+        cout << "3 - Remover Criptomoeda" << endl;
+        cout << "4 - Listar Criptomoedas" << endl;
+        cout << "5 - Sair" << endl;
+        cout << "Opcao: ";
+        cin >> opcao;
+
+        switch (opcao) {
+            case 1:
+                Cadastrar(vetor, totalCriptomoedas, capacidade);
+                salvarCriptomoedas(vetor, totalCriptomoedas);
+                break;
+            case 2:
+                atualizar(vetor, totalCriptomoedas);
+                salvarCriptomoedas(vetor, totalCriptomoedas);
+                break;
+            case 3:
+                remover(vetor, totalCriptomoedas);
+                salvarCriptomoedas(vetor, totalCriptomoedas);
+                break;
+            case 4:
+                listar(vetor, totalCriptomoedas);
+                carregarCriptomoedas(vetor, totalCriptomoedas, capacidade);
+                break;
+            case 5:
+                cout << "Obrigado por usar nosso sistema..." << endl;
+                break;
+            default:
+                cout << "Opcao invalida." << endl;
+        }
+    } while (opcao != 5);
+
+    delete[] vetor; 
+
+    return 0;
+}
